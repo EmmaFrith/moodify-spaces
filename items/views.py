62d096a951b555d2215676370d342a57560d6f2a
Emma Frith <emma.frith@education.gov.uk>
2024-07-07 22:15:34 +0100

    refactor code and add put and delete requests

--- items/views.py
+++ items/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status 
 from .models import Item
 from .serializers.common import ItemSerializer
+from rest_framework.exceptions import NotFound
 
 
 
@@ -22,3 +23,32 @@ class ItemListView(APIView):
         except Exception as e:
             print('ERROR')
             return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)  
+
+
+class ItemDetailView(APIView):
+
+    def get_item(self, pk):
+        try:
+            return Item.objects.get(pk=pk)
+        except Item.DoesNotExist:
+            raise NotFound(detail="Can't find that item.")
+    
+    def get(self, _request, pk):
+        item = self.get_item(pk=pk) 
+        serialized_item = ItemSerializer(item)
+        return Response(serialized_item.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        item_to_update = self.get_item(pk=pk)
+        updated_item = ItemSerializer(item_to_update, data=request.data)
+        try:
+            updated_item.is_valid()
+            updated_item.save()
+            return Response(updated_item.data, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, _request, pk):
+        item_to_delete = self.get_item(pk=pk)
+        item_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
